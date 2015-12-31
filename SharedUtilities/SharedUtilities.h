a6716dc7b8142a7a68cb8b277afefe813db17962
Ian Dundas <contact@iandundas.co.uk>
2015-12-31 14:07:31 +0000

    initial

---
+++ SharedUtilities/SharedUtilities.h
@@ -0,0 +1,19 @@
+//
+//  SharedUtilities.h
+//  SharedUtilities
+//
+//  Created by Ian Dundas on 31/12/2015.
+//  Copyright © 2015 IanDundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedUtilities.
+FOUNDATION_EXPORT double SharedUtilitiesVersionNumber;
+
+//! Project version string for SharedUtilities.
+FOUNDATION_EXPORT const unsigned char SharedUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedUtilities/PublicHeader.h>
+
+
