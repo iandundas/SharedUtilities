0ed5342ecc73224e361f703c8f13450df841ee01
Ian Dundas <contact@iandundas.co.uk>
2015-12-31 14:10:31 +0000

    Added random string func

---
+++ SharedUtilities/Strings.swift
@@ -0,0 +1,20 @@
+//
+//  Strings.swift
+//  SharedUtilities
+//
+//  Created by Ian Dundas on 31/12/2015.
+//  Copyright © 2015 IanDundas. All rights reserved.
+//
+
+import Foundation
+
+extension String{
+    
+    static func random(length length: Int) -> String {
+        let alphabet = "-_1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let upperBound = UInt32(alphabet.characters.count)
+        return String((0..<length).map { _ -> Character in
+            return alphabet[alphabet.startIndex.advancedBy(Int(arc4random_uniform(upperBound)))]
+            })
+    }
+}
